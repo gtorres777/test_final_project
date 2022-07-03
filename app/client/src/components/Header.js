c7ef3735348de0e0e5e7e1021edfd966e81d461c
gtorres777 <gustavo.torres.d@tecsup.edu.pe>
2022-07-03 00:32:41 -0500

    final test

--- app/client/src/components/Header.js
+++ app/client/src/components/Header.js
@@ -4,7 +4,7 @@ const Header = () => {
   return (
         <div className="header">
             <h1>
-                AWS 15 Trainee PERN Stack Example
+                AWS 17 Trainee PERN Stack Example
             </h1>
         </div>
   )
