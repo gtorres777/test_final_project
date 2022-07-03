95bd2097f9aa56303fe1a112116f9324cafa1fff
gtorres777 <gustavo.torres.d@tecsup.edu.pe>
2022-07-02 23:46:44 -0500

    testing the 2 workflows and tests and linter fixed5

--- app/client/src/components/Header.js
+++ app/client/src/components/Header.js
@@ -4,7 +4,7 @@ const Header = () => {
   return (
         <div className="header">
             <h1>
-                AWS 10 Trainee PERN Stack Example
+                AWS 11 Trainee PERN Stack Example
             </h1>
         </div>
   )
