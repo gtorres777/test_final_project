fdc8a21d380a07e1482f7e7eefdd5628cb724088
gtorres777 <gustavo.torres.d@tecsup.edu.pe>
2022-07-08 18:00:42 -0500

    testing microservice cicd

--- react-node-app/microservice/server.js
+++ react-node-app/microservice/server.js
@@ -33,5 +33,5 @@ app.delete('/api/task/:id', (req, res) => {
 })
 
 app.listen(port, () => {
-  console.log(`Server listening on port the ${port}`)
+  console.log(`Stage Server listening on port the ${port}`)
 })
